00463f47c79e850e8c53ecbd84c54b16eb09c137
natekcroteau <natekcroteau@gmail.com>
2021-04-14 14:48:42 -0600

    renders fetched todo data from backend

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import TodoItem from './TodoItem'
+
+export default function TodoContainer({ todos }) {
+
+    const showTodos = () => {
+        return todos.map(todo => <TodoItem key={todo.id} {...todo} />)
+    }
+
+
+    return (
+        <ul className="todo-list">
+            {showTodos()}
+        </ul>
+    )
+}
